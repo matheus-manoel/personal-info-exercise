621b5a7a10c3f319740fdd954c98e1ef6cf1e821
Matheus Manoel <matheuscmanoel@gmail.com>
2020-03-06 16:49:38 -0300

    Finish exercise.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
+const xhr = new XMLHttpRequest();
+
+xhr.onload = () => {
+  console.log(this.responseText);
+}
+
 let postPersonalInfo = () => {
   let payload = {
-    name: document.getElementById('nameInput').value;
-    lastName: document.getElementById('lastNameInput').value;
-    cpf: document.getElementById('cpfInput').value;
-    dateOfBirth: document.getElementById('dateOfBirthInput').value;
-    email: document.getElementById('emailInput').value;
-    phone: document.getElementById('phoneInput').value;
+    firstName: document.getElementById('nameInput').value,
+    lastName: document.getElementById('lastNameInput').value,
+    document: document.getElementById('cpfInput').value,
+    birthDate: document.getElementById('dateOfBirthInput').value,
+    email: document.getElementById('emailInput').value,
+    phone: document.getElementById('phoneInput').value,
   }
+
+  xhr.open('POST', `https://desafio-digital-secomp.firebaseio.com/${payload.document}/dados.json`);
+  xhr.setRequestHeader('Content-type', 'application/json');
+  xhr.send(JSON.stringify(payload));
+}
